d5cc83666c18b8e6b800015b0ebe86d69d97c1ae
greener <654534332@qq.com>
2023-03-27 23:18:26 +0800

    removed @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://greenerdalii.top/",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   })],
@@ -16,6 +17,6 @@ export default defineConfig({
       external: ["svgo"]
     }
   },
-  output: "server",
-  adapter: cloudflare()
+  // output: "server",
+  // adapter: cloudflare()
 });
