acad00af5afd18b0fa7153d68b351ad9950aa241
green-dalii <654534332@qq.com>
2025-07-31 09:24:28 +0800

    remove astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,22 @@ import { defineConfig } from "astro/config";
 import icon from 'astro-icon';
 import remarkGfm from 'remark-gfm';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-
 // import cloudflare from "@astrojs/cloudflare";
-
-
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
-import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: 'file',
+  },
   site: "https://greenerdalii.top/",
-  integrations: [tailwind(), icon(), mdx(), playformCompress({
-    exclude: ['src/data/work/**/*'],
-  })],
+  integrations: [tailwind(), icon(), mdx()],
   vite: {
+    build: {
+      cssMinify: 'lightningcss',
+    },
     ssr: {
       external: ["svgo"]
     }
